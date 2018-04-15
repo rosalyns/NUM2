98417c2bccfd5698a400d587df5238e859bed7e7
Rosalyn Sleurink <rosalyn.sleurink@nedap.com>
2018-04-15 16:02:18 +0200

    Implemented tests a bit

--- src/main/java/tests/UtilsTest.java
+++ src/main/java/tests/UtilsTest.java
@@ -18,34 +18,17 @@ class UtilsTest {
 	}
 
 	@Test
-	final void testGetFileContentsInteger() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
 	final void testGetFileContentsByte() {
 		byte[] file1 = Utils.getFileContents("file5.png", 0);
 		assertEquals(Config.DATASIZE, file1.length);
 		byte[] file2 = Utils.getFileContents("file5.png", Config.DATASIZE);
 		assertEquals(file1.length, file2.length);
-		
-		Integer[] wholeFile = Utils.getFileContents("file5.png");
-		byte[] file = new byte[wholeFile.length];
-		for (int i = 0; i < wholeFile.length; i++) {
-			file[i] = (byte) (int)wholeFile[i];
-		}
+		byte[] file3 = Utils.getFileContents("file5.png", 2*Config.DATASIZE);
 		
 		byte[] file1and2 = Utils.mergeArrays(file1, file2);
+		byte[] file2and3 = Utils.mergeArrays(file2, file3);
 		assertEquals(2 * Config.DATASIZE, file1and2.length);
-		byte[] copyOfFile = new byte[2 * Config.DATASIZE];
-
-		assertEquals(file1and2.length, copyOfFile.length);
-		
-		System.arraycopy(file, 0, copyOfFile, 0, 2*Config.DATASIZE);
-		
-		for (int i = 0; i < file1and2.length; i++) {
-			assertEquals(file1and2[i], copyOfFile[i]);
-		}
+		assertEquals(2 * Config.DATASIZE, file2and3.length);
 	}
 	
 	@Test
@@ -57,12 +40,31 @@ class UtilsTest {
 
 	@Test
 	final void testGetFileSize() {
-		fail("Not yet implemented"); // TODO
+		assertEquals(248, Utils.getFileSize("file1.png"));
+		assertEquals(2085, Utils.getFileSize("file2.png"));
+		assertEquals(6267, Utils.getFileSize("file3.png"));
+		assertEquals(21067, Utils.getFileSize("file4.png"));
+		assertEquals(53228, Utils.getFileSize("file5.png"));
+		assertEquals(216583872, Utils.getFileSize("grand_tour.tif"));
 	}
 
 	@Test
 	final void testMergeArrays() {
-		fail("Not yet implemented"); // TODO
+		byte[] file1 = Utils.getFileContents("file5.png", 0);
+		byte[] file2 = Utils.getFileContents("file5.png", Config.DATASIZE);
+		
+		assertNotEquals(Utils.mergeArrays(file1,file2), Utils.mergeArrays(file2, file1));
+		
+		byte[] data1 = new byte[] { 1,2 };
+		byte[] data2 = new byte[] { 3,4 } ;
+		byte[] result = Utils.mergeArrays(data1, data2);
+		
+		assertEquals(1, result[0]);
+		assertEquals(2, result[1]);
+		assertEquals(3, result[2]);
+		assertEquals(4, result[3]);
+		assertEquals(4, result.length);
+		
 	}
 
 	@Test
